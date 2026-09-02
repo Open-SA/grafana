f2686723977c7e71631f5d915a879a1f5dc860a3
ValFran99 <francovalfre999@gmail.com>
2026-09-02 15:42:31 -0300

    fix: moved the secure config declaration at the begginning of the init function

--- setup.php
+++ setup.php
@@ -70,6 +70,8 @@ function plugin_init_grafana()
     SessionManager::registerPluginStatelessPath('grafana', '#^/front/jwks\.php$#');
 
     $PLUGIN_HOOKS['csrf_compliant']['grafana'] = true;
+    $PLUGIN_HOOKS['secured_configs']['grafana'] = ['password', 'private_key'];
+
     // don't load hooks if plugin not enabled (or glpi not logged)
     if (!Plugin::isPluginActive('grafana') || !Session::getLoginUserID()) {
         return;
@@ -89,9 +91,6 @@ function plugin_init_grafana()
 
     // add dashboards
     Plugin::registerClass(Dashboard::class, ['addtabon' => 'Central']);
-
-    // Encryption
-    $PLUGIN_HOOKS['secured_configs']['grafana'] = ['password', 'private_key'];
 
     // Default central tab — applied once per login session
     $PLUGIN_HOOKS['post_init']['grafana'] = 'plugin_grafana_post_init';
